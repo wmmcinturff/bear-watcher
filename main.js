d5d08f52cc77597a0ee390287143577a5e2720ef
Wanda <wmmcinturff@gmail.com>
2020-08-22 14:19:05 -0500

    modified names

---
+++ main.js
@@ -0,0 +1,29 @@
+const bearArray = [
+    {
+        name: "Bart",
+        image: "bart.jpg",
+    },
+
+    {
+        name: "Winnie",
+        image: "winnie.jpg",
+    },
+    {
+        name: "Kodi",
+        image: "kodi.jpg",
+    },
+    {
+        name: "Sasha",
+        image: "sasha.jpg",
+    },
+    {
+        name: "Stanley",
+        image: "stanley.jpg",
+    },
+    {
+        name: "Barry",
+        image: "barry.jpg",
+    },
+    
+];
+
