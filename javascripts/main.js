09176ad25d9924f2732661cf2d1e51ad8472c4ba
Wanda <wmmcinturff@gmail.com>
2020-09-05 12:28:02 -0500

    resolved loading errors

---
+++ javascripts/main.js
@@ -0,0 +1,15 @@
+import { addBearForm } from './components/bearform.js';
+import { addBearToForm, buildBearCards} from './components/river.js';
+
+
+  const init = () => {
+    addBearForm();
+    addBearToForm();
+    buildBearCards();
+  }       
+  
+  init();
+           
+    
+          
+  
